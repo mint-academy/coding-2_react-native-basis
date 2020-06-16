7673df92515bf0a5f0f17b87566e5d806a030d36
Fabio A. Camichel <fabiocamichel@gmail.com>
2020-06-16 06:39:19 +0200

    feat: finished basic app
    using react-navigation and react-native-elements

--- App.js
+++ App.js
@@ -1,67 +1,195 @@
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
-import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+// React Native
+import * as React from 'react';
+import { View, StyleSheet } from 'react-native';
+// Iconsset: https://iconify.design/icon-sets/ion/
 import { Ionicons } from '@expo/vector-icons';
+// https://reactnavigation.org
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// https://react-native-elements.github.io/react-native-elements/docs/1.2.0/overview.html
+import { CheckBox, Header, Text, Divider } from 'react-native-elements';
 
-import AppNavigator from './navigation/AppNavigator';
 
-  /*
-   TodoApp: Haupteinstiegpunkt
-     render -> ListView wird angezeigt
-  */
+// Funktion um das Icon mit der Zahl zu erstellen
+// https://reactnavigation.org/docs/tab-based-navigation
+function IconWithBadge({ name, badgeCount, color, size }) {
+  return (
+    <View style={{ width: 24, height: 24, margin: 5 }}>
+      <Ionicons name={name} size={size} color={color} />
+      {badgeCount > 0 && (
+        <View
+          style={{
+            position: 'absolute',
+            right: -6,
+            top: -3,
+            backgroundColor: 'red',
+            borderRadius: 6,
+            width: 12,
+            height: 12,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Text style={{ color: 'white', fontSize: 10, fontWeight: 'bold' }}>
+            {badgeCount}
+          </Text>
+        </View>
+      )}
+    </View>
+  );
+}
 
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
+// Beispiel für zwei Neuigkeiten
+// die Konstante "news" ist ein Array mit zwei Neuigkeiten
+// Diese Informationen würden genau in diesem Format von einer Schnittstelle her kommen (Server)
+const news = [
+ {
+    title: 'Schulhaus Daleu wieder offen',
+    text: 'Ein interessanter Text darüber, dass das Schulhaus Daleu in Chur wieder offen ist.'
+ },
+ {
+   title: 'Keine mündlichen und schriftlichen Prüfungen mehr',
+   text: 'Alle Informationen zur Prüfungsverordnung bis Sommer 2020.'
+ }
+]
 
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return (
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
+function HomeIconWithBadge(props) {
+  // Der badgeCount steuert die kleine Zahl. Diese Zahl kommt normalerweise vom Server.
+  return <IconWithBadge {...props} badgeCount={news.length} />;
+}
+
+// Home-Tab
+function HomeScreen() {
+  return (
+    <View>
+      <Header
+        centerComponent={{ text: 'Home', style: styles.header }}
       />
-    );
-  } else {
+      <Text h1 style={ styles.h1 }>Neuigkeiten</Text>
+      {/*
+        <Text h3 style={ styles.h3 }>{news[0].title}</Text>
+        <Text style={ styles.block }>{news[0].text}</Text>
+      */}
+      {
+        /*
+          https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+        */
+        news.map(e => {
+          return (
+            <View>
+              <Text h3 style={ styles.h3 }>{e.title}</Text>
+              <Text style={ styles.block }>{e.text}</Text>
+            </View>
+          )
+        })
+      } 
+    </View>
+  );
+}
+
+// Aufgaben-Tab
+class TasksScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkboxes: [{
+        id: 1,
+        title: 'Repetition English Wörter',
+        checked: false,
+      }, {
+        id: 2,
+        title: 'Fussballschuhe kaufen',
+        checked: true,
+      }],
+    };
+  }
+
+  render() {
     return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
+      <View>
+        <Text h1>Aufgaben</Text>
+        {
+          this.state.checkboxes.map(e => {
+            return (
+              <CheckBox
+                left
+                title={e.title}
+                checked={e.checked}
+              />
+            )
+          })   
+        }     
       </View>
     );
   }
 }
 
-async function loadResourcesAsync() {
-  await Promise.all([
-    Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
-    ]),
-    Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    }),
-  ]);
+const Tab = createBottomTabNavigator();
+
+class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              if (route.name === 'Home') {
+                return (
+                  <HomeIconWithBadge
+                    name={
+                      focused
+                        ? 'ios-information-circle'
+                        : 'ios-information-circle-outline'
+                    }
+                    size={size}
+                    color={color}
+                  />
+                );
+              } else if (route.name === 'Aufgaben') {
+                return (
+                  <Ionicons
+                    name={focused ? 'ios-list-box' : 'ios-list'}
+                    size={size}
+                    color={color}
+                  />
+                );
+              }
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+          }}
+        >
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Aufgaben" component={TasksScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
-function handleLoadingError(error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
-  console.warn(error);
-}
+// Einstiegspunkt der App
+export default App
 
-function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
-}
-
+// Styling der App
+// https://www.w3schools.com/css/default.asp
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  header: {
+    color: "white",
+    fontSize: 20,
+    textTransform: "uppercase"
   },
+  h1: {
+    margin: 10
+  },
+  h3: {
+    margin: 15,
+    color: "grey"
+  },
+  block: {
+    marginLeft: 15,
+    marginRight: 15,
+    paddingBottom: 30
+  }
 });
